2413d98dbd59daa883c606e79400975cc82ddce4
ilhamsurya <ilhamsurya.m@gmail.com>
2024-04-30 17:09:21 +0700

    add base config and init main.go

---
+++ cmd/app/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"context"
+	"projectsphere/cats-social/pkg/middleware/graceful"
+	"projectsphere/cats-social/pkg/middleware/logger"
+	"projectsphere/cats-social/pkg/protocol/httpListener"
+	"time"
+)
+
+func main() {
+	logger.InitLogger()
+
+	httpProtocol := httpListener.Start()
+	graceful.GracefulShutdown(
+		context.TODO(),
+		time.Duration(5*time.Second),
+		map[string]graceful.Operation{
+			"http": func(ctx context.Context) error {
+				return httpProtocol.Shutdown(ctx)
+			},
+		},
+	)
+
+	httpProtocol.Listen()
+}
